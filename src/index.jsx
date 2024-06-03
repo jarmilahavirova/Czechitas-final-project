8aea7ab9d963b223214e61249e8175ae05e6c321
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-06-03 04:30:59 +0200

    React Router implementation

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,32 @@
-import { createRoot } from 'react-dom/client';
-import { HomePage } from './pages/HomePage';
-import './global.css';
+import "./global.css";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { ErrorPage } from "./pages/ErrorPage";
+import { GamePage } from "./pages/GamePage";
+import { ResultPage } from "./pages/ResultPage";
+import { AboutPage } from "./pages/AboutPage";
 
-createRoot(
-  document.querySelector('#app'),
-).render(<HomePage />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/game",
+    element: <GamePage />,
+  },
+  {
+    path: "/result",
+    element: <ResultPage />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+]);
+
+createRoot(document.querySelector("#app")).render(
+  <RouterProvider router={router} />
+);
