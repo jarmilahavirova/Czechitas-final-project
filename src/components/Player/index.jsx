c870cfb9d9868b4871737c38b45ea0d281fc94fd
JanaDolejsi <jana.dolejsi.gr@gmail.com>
2024-06-08 14:20:16 +0200

    Select avatar

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -1,20 +1,43 @@
-import head1 from "../../../assets/head1.png"
-import head2 from "../../../assets/head2.png"
-import head3 from "../../../assets/head3.png"
-import head4 from "../../../assets/head4.png"
-import "./style.css"
+import head1 from "../../../assets/head1.png";
+import head2 from "../../../assets/head2.png";
+import head3 from "../../../assets/head3.png";
+import head4 from "../../../assets/head4.png";
+import "./style.css";
+import { useState } from "react";
 
-export const Player = ({playersNumber}) => {
-    return (
-        <div className="player">
-            <p>Hráč {playersNumber}</p>
-            <div>
-                <img src={head1} alt="" />
-                <img src={head2} alt=""/>
-                <img src={head3} alt=""/>
-                <img src={head4} alt=""/>
-                <input></input>
-            </div>
-        </div>
-    )
-}
+export const Player = ({ playersNumber }) => {
+  const [avatar, setAvatar] = useState("");
+
+  return (
+    <div className="player">
+      <p>Hráč {playersNumber}</p>
+      <div>
+        <img
+          className={avatar === "head1" ? "selected" : ""}
+          src={head1}
+          alt=""
+          onClick={() => setAvatar("head1")}
+        />
+        <img
+          className={avatar === "head2" ? "selected" : ""}
+          src={head2}
+          alt=""
+          onClick={() => setAvatar("head2")}
+        />
+        <img
+          className={avatar === "head3" ? "selected" : ""}
+          src={head3}
+          alt=""
+          onClick={() => setAvatar("head3")}
+        />
+        <img
+          className={avatar === "head4" ? "selected" : ""}
+          src={head4}
+          alt=""
+          onClick={() => setAvatar("head4")}
+        />
+        <input></input>
+      </div>
+    </div>
+  );
+};
