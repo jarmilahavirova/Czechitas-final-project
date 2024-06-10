8189c41f89d671b1409b297a986ef231112c2bee
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-06-10 03:27:20 +0200

    Change of Header component - Icon buttons, type link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,11 +5,11 @@ import "./style.css";
 export const Header = () => {
   return (
     <header className="header">
-      <Link to="/">
-        <IconButton icon="/icon-home.png" />
-      </Link>
+      <IconButton type="link" icon="/icon-home.png" link="/" />
+
       <h1 className="header__title">RULES OPEN 2024</h1>
-      <IconButton icon="/icon-info.png" />
+
+      <IconButton type="link" icon="/icon-info.png" link="/about" />
     </header>
   );
 };
