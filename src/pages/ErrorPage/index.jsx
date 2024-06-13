4c6a28d47a3d146da3d9848be87d290902854e93
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-06-13 17:56:17 +0200

    Error Page start

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,5 +1,24 @@
 import "./style.css";
+import lostDisc from "../../../assets/lost_disc.png";
+import { Link } from "react-router-dom";
 
 export const ErrorPage = () => {
-  return <></>;
+  return (
+    <>
+      <main className="error">
+        <h2 className="error__title">404</h2>
+        <p className="error__message">
+          Tady jsi ztracený jak disk v běchovických křovinách.
+        </p>
+        <Link className="error__button" to="/">
+           PŘEJÍT NA HLAVNÍ STRÁNKU
+        </Link>
+        <img
+          src={lostDisc}
+          alt="Ilustrační obrázek ztraceného disku"
+          className="error__picture"
+        />
+      </main>
+    </>
+  );
 };
